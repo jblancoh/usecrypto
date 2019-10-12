34a895af5ab83069dd9becb17f111483af816e96
Jonathan Blanco <zlatanjon@hotmail.com>
2019-10-11 20:06:39 -0500

    add gh corner

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import React from "react"
 
 import Layout from "../components/Layout"
 import EntryList from "../components/EntryList"
+import GHCorner from 'react-gh-corner';
 
 const data = require("../../data/data.json")
 
 const IndexPage = () => {
   return (
     <Layout>
+      <GHCorner href="https://github.com/deamme/usecrypto" />
       <h1 className="title">Use Crypto</h1>
       <EntryList entries={data} />
     </Layout>
